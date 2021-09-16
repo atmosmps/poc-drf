90c0f25f6ae1d734281d34021a4c7153389bf478
atmosmps <atmos.souza@luizalabs.com>
2021-09-15 22:22:52 -0300

    add app resources

--- app/settings.py
+++ app/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'core'
+    'core',
+    'resources',
 ]
 
 MIDDLEWARE = [
--- core/models.py
+++ core/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-
-# Create your models here.
+from resources.models import Resource
 
 
 class TouristPlaces(models.Model):
     name = models.CharField(max_length=150)
     description = models.TextField()
     approved = models.BooleanField(default=False)
+    resources = models.ManyToManyField(Resource)
 
     def __str__(self):
         return self.name
