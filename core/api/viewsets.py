c54f0a6041895389e863a6eb3ab159e3b9adde87
atmosmps <atmos.souza@luizalabs.com>
2021-09-20 21:58:47 -0300

    update queryset method

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -10,7 +10,19 @@ class TouristPlaceViewSet(ModelViewSet):
     serializer_class = TouristPlaceSerializer
 
     def get_queryset(self):
-        return TouristPlace.objects.all()
+        id = self.request.query_params.get['id', None]
+        name = self.request.query_params.get['name', None]
+        description = self.request.query_params.get['description', None]
+
+        queryset = TouristPlace.objects.all()
+
+        if id:
+            queryset = queryset.filter(pk=id)
+        if name:
+            queryset = queryset.filter(name__iexact=name)
+        if description:
+            queryset = queryset.filter(description__iexact=description)
+        return queryset
 
     # detail=True, especifa que o pk/id do recurso que será acessado.
     # Caso contrário faz referência ao acesso global do recurso.
