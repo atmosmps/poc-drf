5892868f399c0e8226b30ff947913bbbb8cd4650
atmosmps <atmos.souza@luizalabs.com>
2021-10-26 08:30:16 -0300

    chore: add obs about search and lookup_field

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -9,8 +9,26 @@ from core.models import TouristPlace
 class TouristPlaceViewSet(ModelViewSet):
     """A simple ViewSet for viewing and editing accounts."""
     serializer_class = TouristPlaceSerializer
+
+    """
+    Habilita a busca por search e se baseia pelos campos definidos em
+    "search_fields" para realizar a busca. Não é performático. Porém com esta
+    definição de busca é possível utilizar os "lookup_prefixes" definidos em
+    SearchFilter.
+    """
     filter_backends = (SearchFilter,)
     search_fields = ('name', 'description')
+
+    """
+    Substitui o comportamento padrão de busca pelo id do model, e passa a
+    realizar a busca principal pelo campo definido em "lookup_field", que por
+    padrão é o ID do model.
+    
+    OBS: o campo definido em "lookup_field" precisa ser exclusivo(unique), caso
+    contrário será retornado um exceção: MultipleObjectsReturned
+    
+    lookup_field = 'name'
+    """
 
     def get_queryset(self):
         id = self.request.query_params.get('id', None)
