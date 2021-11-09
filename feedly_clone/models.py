87021af5011e56ba0a56e9f34410124f1d4d3c3e
atmosmps <atmos.souza@luizalabs.com>
2021-11-08 21:12:08 -0300

    chore: define models

--- feedly_clone/models.py
+++ feedly_clone/models.py
@@ -1,2 +1,21 @@
 from django.db import models
 
+
+class Channel(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    link = models.URLField()
+
+    def __str__(self):
+        return f"Channel - title: {self.title}"
+
+
+class Item(models.Model):
+    channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
+    title = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    link = models.URLField()
+    pub_date = models.DateTimeField()
+
+    def __str__(self):
+        return f"Item - title: {self.title}"
