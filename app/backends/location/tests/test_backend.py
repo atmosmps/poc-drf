2988d6e8d16dfa343fa5be171e2a1f94e8ce83a4
atmosmps <atmos.souza@luizalabs.com>
2022-04-21 20:17:15 -0300

    test: add backends test

---
+++ app/backends/location/tests/test_backend.py
@@ -0,0 +1,8 @@
+import pytest
+from app.backends.location.backend import LocationBackend
+
+
+class TestLocationBackend(object):
+    def test_location_backend_class_abstraction(self):
+        with pytest.raises(TypeError):
+            LocationBackend()
---
+++ app/backends/location/tests/test_exceptions.py
@@ -0,0 +1,9 @@
+from app.backends.checkout.exceptions import CheckoutBackendException
+from app.backends.location.exceptions import LocationBackendException
+
+
+class TestLocationBackendException:
+    def test_init_method(self):
+        message = "some gibberish"
+        exception = LocationBackendException(message=message)
+        assert exception.message == message
